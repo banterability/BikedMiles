ed5f1b58d69fc6fc2fdf2ba94adaceb41541f945
Jeff Long <banterability@users.noreply.github.com>
2024-03-02 08:01:56 -1000

    Add Sentry

--- BikedMiles/BikedMilesApp.swift
+++ BikedMiles/BikedMilesApp.swift
@@ -1,7 +1,20 @@
 import SwiftUI
+import Sentry
+
 
 @main
 struct BikedMilesApp: App {
+    init() {
+        SentrySDK.start { options in
+            options.dsn = "https://86a3afda561178c4d25346af26150727@o33492.ingest.us.sentry.io/4506842707656704"
+            options.debug = true // Enabled debug when first installing is always helpful
+            options.enableTracing = true 
+
+            // Uncomment the following lines to add more data to your events
+            // options.attachScreenshot = true // This adds a screenshot to the error events
+            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+        }
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
